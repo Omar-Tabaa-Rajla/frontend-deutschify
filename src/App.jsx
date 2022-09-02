77388d3180237e353bb90eb3efa8390f3c0aa027
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-09-02 19:02:26 +0200

    route and states in app.jsx importiert

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div className="App">
             <h1 className="text-3xl font-bold underline">
